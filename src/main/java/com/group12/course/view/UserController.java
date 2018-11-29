0a688edd71afecad92f5ea8f87206115a5bf7651
Xu Gang <xugang9804@outlook.com>
2018-11-29 19:57:00 +0800

    针对规约修改注释，更改了获取用户的方式，改为GET

--- src/main/java/com/group12/course/view/UserController.java
+++ src/main/java/com/group12/course/view/UserController.java
@@ -5,11 +5,11 @@ import com.group12.course.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-/*
+/**
  * User controller
  * @author Xu Gang
  * @date 2018年11月29日
- */
+*/
 
 @RestController
 
@@ -19,10 +19,9 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @ResponseBody
-    @PostMapping(value = "/login",produces = "application/json")
-    public User getUserByAccount(@RequestBody User user){
-        return userService.getUserByAccount(user.getAccount());
+    @GetMapping(value = "/login/{account}")
+    public User getUserByAccount(@PathVariable String account){
+        return userService.getUserByAccount(account);
     }
 
 }
