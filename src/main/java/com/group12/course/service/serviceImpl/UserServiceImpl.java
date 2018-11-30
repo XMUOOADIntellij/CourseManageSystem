d90c31bd2d4def245f932e1e58ca829a91dd2195
Xu Gang <xugang9804@outlook.com>
2018-11-30 12:44:17 +0800

    修复当用户不在数据库中会出错的情况

--- src/main/java/com/group12/course/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/group12/course/service/serviceImpl/UserServiceImpl.java
@@ -27,7 +27,9 @@ public class UserServiceImpl implements UserService {
     @Override
     public User checkUser(String account,String password) {
         User user = userDao.getUserByAccount(account);
-        if (user.getPassword().equals(password)){
+
+        // 若该用户在数据库中不存在，则 user == null，要剔除这种情况
+        if ( user != null && user.getPassword().equals(password)){
             return user;
         }
         else{
