9e1a9704e9035679005eb9506527d513b1115135
Xu Gang <xugang9804@outlook.com>
2018-12-11 23:00:05 +0800

    创建所有 controller 接口

--- src/main/java/com/group12/course/controller/AdminController.java
+++ src/main/java/com/group12/course/controller/AdminController.java
@@ -1,9 +1,33 @@
 package com.group12.course.controller;
 
+import com.group12.course.entity.Teacher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Admin controller
  * @author Xu Gang
  * @date 2018年12月11日
  * */
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+
+   /* @Autowired
+    AdminService adminService;*/
+   /*TODO*/
+
+    @PostMapping(value = "/login",produces = "application/json; charset=utf-8")
+    public void login(@RequestBody Teacher user, HttpServletResponse response)throws IOException {
+        /*TODO*/
+    }
+
+
 }
