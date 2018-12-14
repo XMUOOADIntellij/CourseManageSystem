a20a2eae1cea8d873445a294740ac949b3efb9ea
isNxt <36531399+isNxt@users.noreply.github.com>
2018-12-14 10:34:01 +0800

    add course-grade page

--- src/main/resources/mobile/assets/js/common.js
+++ src/main/resources/mobile/assets/js/common.js
@@ -36,7 +36,7 @@ function createUser() {
     return true; // 必须返回false，否则表单会自己再做一次提交操作，并且页面跳转
 }
 
-function createStudent() {
+function createCourse() {
 // jquery 表单提交
     alert("success");
     return true; // 必须返回false，否则表单会自己再做一次提交操作，并且页面跳转
