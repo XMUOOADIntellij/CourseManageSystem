efa8fe9bcb80c45115756bd0c5db72efa096907d
isNxt <36531399+isNxt@users.noreply.github.com>
2018-12-15 16:50:43 +0800

    add seminar detail page

--- src/main/resources/mobile/assets/js/common.js
+++ src/main/resources/mobile/assets/js/common.js
@@ -25,6 +25,11 @@ function updateSeminar() {
   alert("success");
   return true; // 必须返回false，否则表单会自己再做一次提交操作，并且页面跳转
 }
+function updateRound() {
+  // jquery 表单提交
+  alert("success");
+  return true; // 必须返回false，否则表单会自己再做一次提交操作，并且页面跳转
+}
 function updateEmail() {
   // jquery 表单提交
   alert("success");
