8ad1b35e3d5150cb2625f68cc97552fd4dfcc14b
Xu Gang <xugang9804@outlook.com>
2018-12-17 17:32:07 +0800

    添加组队策略

---
+++ src/main/java/com/group12/course/entity/strategy/CourseMemberLimitStrategy.java
@@ -0,0 +1,52 @@
+package com.group12.course.entity.strategy;
+
+import com.group12.course.entity.Course;
+
+/**
+ * 选某课程人数限制策略
+ *
+ * @author Xu Gang
+ * @date 2018年12月17日
+ */
+public class CourseMemberLimitStrategy extends Strategy {
+
+    private Course course;
+
+    private Integer minMember;
+
+    private Integer maxMember;
+
+    public CourseMemberLimitStrategy() {
+    }
+
+    public CourseMemberLimitStrategy(Long id, Course course, Integer minMember, Integer maxMember) {
+        super(id);
+        this.course = course;
+        this.minMember = minMember;
+        this.maxMember = maxMember;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Integer getMinMember() {
+        return minMember;
+    }
+
+    public void setMinMember(Integer minMember) {
+        this.minMember = minMember;
+    }
+
+    public Integer getMaxMember() {
+        return maxMember;
+    }
+
+    public void setMaxMember(Integer maxMember) {
+        this.maxMember = maxMember;
+    }
+}
