875b6c90b2439a7127521a8abaddd3eb9428a4e4
TXueXue <43594603+TXueXue@users.noreply.github.com>
2018-12-19 10:40:07 +0800

    修改RoundController

--- src/main/java/com/group12/course/vo/RoundVO.java
+++ src/main/java/com/group12/course/vo/RoundVO.java
@@ -1,15 +1,13 @@
 package com.group12.course.vo;
 
 import com.group12.course.entity.Course;
+import com.group12.course.entity.KlassRound;
+import com.group12.course.entity.Round;
 import com.group12.course.entity.Seminar;
 
 import java.util.List;
 
 public class RoundVO {
-
-    private Long id;
-
-    private Integer roundSerial;
 
     private Integer presentationScoreMethod;
 
@@ -17,8 +15,35 @@ public class RoundVO {
 
     private Integer questionScoreMethod;
 
-    private List<Seminar> seminarList;
+    public Integer getPresentationScoreMethod() {
+        return presentationScoreMethod;
+    }
 
-//    private List<klassRound> klassRoundList;
-//
+    public void setPresentationScoreMethod(Integer presentationScoreMethod) {
+        this.presentationScoreMethod = presentationScoreMethod;
+    }
+
+    public Integer getReportScoreMethod() {
+        return reportScoreMethod;
+    }
+
+    public void setReportScoreMethod(Integer reportScoreMethod) {
+        this.reportScoreMethod = reportScoreMethod;
+    }
+
+    public Integer getQuestionScoreMethod() {
+        return questionScoreMethod;
+    }
+
+    public void setQuestionScoreMethod(Integer questionScoreMethod) {
+        this.questionScoreMethod = questionScoreMethod;
+    }
+
+    public RoundVO(){}
+
+    public RoundVO(Round round){
+        this.presentationScoreMethod = round.getPresentationScoreMethod();
+        this.reportScoreMethod = round.getReportScoreMethod();
+        this.questionScoreMethod = round.getQuestionScoreMethod();
+    }
 }
