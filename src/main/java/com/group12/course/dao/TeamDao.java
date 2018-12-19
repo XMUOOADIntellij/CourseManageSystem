7e570b9cfc8277d736057e9ab7deb71bcbc3c829
Xu Gang <xugang9804@outlook.com>
2018-12-19 18:14:28 +0800

    添加 toString 方法，便于调试

--- src/main/java/com/group12/course/dao/TeamDao.java
+++ src/main/java/com/group12/course/dao/TeamDao.java
@@ -7,6 +7,7 @@ import com.group12.course.mapper.TeamMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -22,10 +23,17 @@ public class TeamDao {
     @Autowired
     TeamMapper teamMapper;
 
-    /*public Team checkStudentInTeam(Student student){
-        Team team=getTeamByStudentId(student.getId());
-    }*/
+    @Autowired
+    StudentDao studentDao;
 
+    /**
+     * 检查队伍信息是否合法
+     * 其中检查 course klass leader 是否为空
+     *
+     * @param team 传入待检查的队伍对象
+     * @return 若均不为空，返回 true
+     * 否则为 false
+     * */
     public Boolean checkTeamValid(Team team){
         try {
             Long courseId = team.getCourse().getId();
@@ -33,49 +41,105 @@ public class TeamDao {
             Long leaderId = team.getLeader().getId();
         }
         catch (NullPointerException e){
-            //throw team invalid Exception
+            // throw team invalid Exception
             System.out.println("team message invalid");
             return false;
         }
         return true;
     }
 
+    /**
+     * 检查传入的队伍的队长是否在队伍内
+     * 若在队内的话会抛出异常
+     *
+     * @param team 传入待检查的队伍对象
+     * @return 若不在，返回 true
+     * 否则为 false
+     * */
     public Boolean checkLeaderInTeam(Team team){
-        Team leaderInTeam =getTeamByStudentId(team.getLeader().getId());
-        if (leaderInTeam!=null){
+        if (checkStudentIsInTeam(team.getLeader())){
             //throw leader already in team exception
-            System.out.println("leader is already in a team \n"+leaderInTeam);
+            System.out.println("leader is already in a team \n");
             return false;
         }
         return true;
     }
 
+    /**
+     * 检查传入的学生是否在某个队伍内
+     * 若在队内的话会抛出异常
+     *
+     * @param student 传入待检查的队伍对象
+     * @return 若不在，返回 true
+     * 否则为 false
+     * */
+    public Boolean checkStudentIsInTeam(Student student){
+        Team team = getTeamByStudentId(student.getId());
+        if (team.getId()==null){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * 检查传入的队伍的队员是否在队伍内
+     * 若在队内的话会抛出异常
+     *
+     * @param team 传入待检查的队伍对象
+     * @return 若不在，返回 true
+     * 否则为 false
+     * */
     public Boolean checkMembersInTeam(Team team){
         Iterator<Student> iterator = team.getMembers().iterator();
         while (iterator.hasNext()){
             Student member = iterator.next();
-            Long memberInTeam = teamMapper.selectTeamIdByMembersId(member.getId());
-            if (memberInTeam!=null){
+            if (checkStudentIsInTeam(team.getLeader())){
                 // throw members already in team exception
-                System.out.println("member \n"+ member + "\n is already in a team \n"+memberInTeam);
+                System.out.println("member \n"+ member + "\n is already in a team \n");
                 return false;
             }
         }
         return true;
     }
 
+    /**
+     * 根据 id 获取队伍
+     *
+     * @param account 队伍id
+     * @return 返回一个包含所有队员的队伍*/
     public Team getTeamById(Long account){
-        return teamMapper.selectTeamById(account);
+        Team team = teamMapper.selectTeamById(account);
+        return getMembers(team);
     }
 
+    /**
+     * 根据队长 id 获取队伍对象
+     *
+     * @param id 队长的 id
+     * @return 以该 id 为队长的队伍
+     * */
     public Team getTeamByLeaderId(Long id){
         return teamMapper.selectTeamByLeaderId(id);
     }
 
+    /**
+     * 根据队员 id 获取队伍id
+     *
+     * @param id 队员的id
+     * @return 该 id 所在的队伍的id
+     * */
     public Long getTeamByMembersId(Long id){
         return teamMapper.selectTeamIdByMembersId(id);
     }
 
+    /**
+     * 根据队伍id 删除小组
+     *
+     * @param teamId 队伍的id
+     * @return 该 id 所在的队伍的id
+     * */
     public int deleteTeamById(Long teamId){
         int deleteTeamCount=teamMapper.deleteTeam(teamId);
         if (deleteTeamCount==1){
@@ -85,10 +149,23 @@ public class TeamDao {
         return -1;
     }
 
+    /**
+     * 将某个学生从队伍内移除
+     *
+     * @param member 将要移除的学生对象
+     * @param team  要移除出的队伍
+     * @return 删除数量
+     * */
     public int deleteTeamMember(Team team,Student member){
         return teamMapper.deleteTeamMembers(member.getId());
     }
 
+    /**
+     * 添加某个队伍
+     *
+     * @param team 新加的队伍对象
+     * @return 返回的队伍对象中包含新添加的对象的id
+     * */
     public Team addTeam(Team team){
         if (!checkTeamValid(team)||!checkLeaderInTeam(team)||!checkMembersInTeam(team)){
             return new Team();
@@ -124,9 +201,17 @@ public class TeamDao {
         return team;
     }
 
+    /**
+     * 这个方法用于给现有的队伍添加组员
+     *
+     * @param team 新的队伍
+     * @param member 组员
+     * @return 返回新的队伍对象
+     * */
     public Team addTeamMembers(Team team,Student member){
         team=teamMapper.selectTeamById(team.getId());
-        if (!checkTeamValid(team)){
+        Long memberInTeam = teamMapper.selectTeamIdByMembersId(member.getId());
+        if (!checkTeamValid(team)||memberInTeam!=null){
             // throw team not exist exception
             return new Team();
         }
@@ -139,10 +224,22 @@ public class TeamDao {
         return team;
     }
 
+    /**
+     * 这个方法用于给更改队伍状态
+     *
+     * @param team 传入的队伍
+     * @return 修改数量
+     * */
     public int changeTeam(Team team){
         return teamMapper.updateTeam(team);
     }
 
+    /**
+     * 根据传入的学生 id
+     * 获取其所在的队伍（以队长身份或队员身份的都算）
+     *
+     * @param id 查询的学生id
+     * @return 查询到的队伍对象*/
     public Team getTeamByStudentId(Long id){
         Team teamByLeader = getTeamByLeaderId(id);
         if (teamByLeader==null){
@@ -152,7 +249,6 @@ public class TeamDao {
             }
             else {
                 Team teamByMember = teamMapper.selectTeamById(teamIdByMembers);
-
                 return getMembers(teamByMember);
             }
         }
@@ -161,12 +257,27 @@ public class TeamDao {
         }
     }
 
+    /**
+     * 给传入的队伍对象添加组员
+     *
+     * @param team 传入的队伍对象
+     * @return 队伍对象*/
     public Team getMembers(Team team){
         if (!checkTeamValid(team)){
             return new Team();
         }
         List<Student> members = teamMapper.selectTeamMembersByTeamId(team.getId());
-        team.setMembers(members);
+        List<Student> realMembers = new ArrayList<>();
+        Iterator<Student> iterator =members.iterator();
+        while (iterator.hasNext()){
+            Student member = iterator.next();
+            Student temp = studentDao.getStudentById(member.getId());
+            if (temp.getAccount()!=null){
+                member=temp;
+            }
+            realMembers.add(member);
+        }
+        team.setMembers(realMembers);
         return team;
     }
 }
