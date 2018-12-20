4c227ea2cf174a47ffff385e01485290fa171121
isNxt <36531399+isNxt@users.noreply.github.com>
2018-12-20 09:53:33 +0800

    re-organize the front structure

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,12 +1,32 @@
 function bindUser() {
   // jquery 表单提交
-  alert("success");
-  return true; // 必须返回false，否则表单会自己再做一次提交操作，并且页面跳转
-}
-function createUser() {
-  // jquery 表单提交
-  alert("success");
-  return true; // 必须返回false，否则表单会自己再做一次提交操作，并且页面跳转
+  // let ata = {account:$("#account").val(),password:$("#password").val()}
+  let ata = {account:"213",password:"123456"};
+  $.ajax({
+    type:'post',
+    url: 'http://xug98.cn/user/login',
+    dataType: "json",
+    data: JSON.stringify(ata),
+    contentType: "application/json",
+    success: function (data,textStatus,xhr) {
+      console.log(data);
+      alert("success");
+      if(xhr.status === 200){//状态码存疑
+        window.localStorage.setItem("jwt", data.jwt);
+        if(data.type === "student")
+          window.location.href="/student/profile";
+        else
+          window.location.href="/teacher/home";
+      }
+    },
+    statusCode:{
+      400: function () {
+        $("#password").val("");
+        alert("用户名或密码错误！");
+      }
+    }
+  });
+  return false; // 必须返回false，否则表单会自己再做一次提交操作，并且页面跳转
 }
 
 function createPassword() {
---
+++ src/main/resources/static/js/examples/common.js
@@ -0,0 +1,119 @@
+
+/*------------------------AccountLoginPage---------------------------------*/
+$(function () {
+    $("#submit").click(function () {
+        let ata = {phone:$("#username").val(),password:$("#password").val()}
+        $.ajax({
+            type:'post',
+            url: '/auth',
+            dataType: "json",
+            data: JSON.stringify(ata),
+            contentType: "application/json",
+            success: function (data,textStatus,xhr) {
+                if(xhr.status === 200){//状态码存疑
+                    window.localStorage.setItem("jwt", data.jwt);
+                    if(data.)
+                    if(data.type === "student")
+                        window.location.href="/student/profile";
+                    else
+                        window.location.href="/teacher/home";
+                }
+            },
+            statusCode:{
+                401: function () {
+                    $("#password").val("");
+                    alert("用户名或密码错误！");
+                }
+            }
+        });
+    });
+});
+/*------------------------Register---------------------------------*/
+function registerload(){
+     $.ajax({
+    type:'get',
+    url: '/school/province',
+    dataType: "json",
+    contentType: "application/json;",
+    success: function (data,textStatus,xhr) {
+        if(xhr.status === 200){
+            let content = document.getElementById("pro");
+            let str = "";
+            for(i=0;i<data.length;i++){
+                str = str +"<option>"+data[i]+"</option>"
+            }
+            content.innerHTML = str;
+            }
+    },
+    statusCode: {
+        400: function () {
+            alert("错误的ID格式");
+        },
+        404: function () {
+            alert("未找到话题");
+        }
+    }
+});
+
+}
+
+function find_city(){
+    $.ajax({
+        type: "get",
+        url: "/school/city?province="+$("#pro").find("option:selected").text(),
+        success: function(data) {
+            let content = document.getElementById("city");
+            let str = "";
+            for(i=0;i<data.length;i++){
+                str = str +"<option>"+data[i]+"</option>"
+            }
+            content.innerHTML = str;
+        }
+    });
+}
+
+function find_school(){
+       $.ajax({
+        type: "get",
+        url: "/school?city="+$("#city").find("option:selected").text(),
+        success: function(data) {
+            let content = document.getElementById("school");
+            let str = "";
+            for(i=0;i<data.length;i++){
+                str = str +"<option>"+data[i].name+"</option>"
+            }
+            content.innerHTML = str;
+        }
+    });
+}
+
+function submitregister(){
+ let ata = {
+    phone:$("#phone").val(),
+    password:$("#password").val(),
+    name:$("#name").val(),
+    school:$("#school").val(),
+    gender:$('.male > input:radio:checked').val(),
+    type:($('.student > input:radio:checked').val() === "学生")?0:1,
+    number:$("#number").val(),
+    email:$("#eMail").val()
+    }
+    $.ajax({
+        type:'post',
+        url: '/auth/register',
+        dataType: "json",
+        data: JSON.stringify(ata),
+        contentType: "application/json",
+        success: function (data,textStatus,xhr) {
+            if(xhr.status === 200){
+                alert("注册成功");
+                window.location.href="/login";
+            }
+        },
+        statusCode:{
+            401: function () {
+                alert("无法注册！");
+            }
+        }
+    });
+}
