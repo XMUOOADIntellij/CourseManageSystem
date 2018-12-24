07dc16f5630166c6628d39292088a11d71e7aea2
Xu Gang <xugang9804@outlook.com>
2018-12-25 01:13:50 +0800

    添加 jwt 验证

--- src/main/java/com/group12/course/interceptor/AOP.java
+++ src/main/java/com/group12/course/interceptor/AOP.java
@@ -3,21 +3,26 @@ package com.group12.course.interceptor;
 import com.group12.course.entity.Student;
 import com.group12.course.entity.Teacher;
 import com.group12.course.tools.Jwt;
+import org.apache.commons.lang3.time.StopWatch;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
-import java.util.logging.Logger;
 
 
 @Aspect
 @Component
 public class AOP {
 
+    private final Logger logger = LoggerFactory.getLogger(AOP.class);
     /**
      * 拦截 controller 包下所以方法
      */
@@ -30,10 +35,10 @@ public class AOP {
     /**
      * 前置通知
      *
-     * @param joinPoint
+     * @param proceedingJoinPoint
      */
     @Around("log()")//log()方法之前
-    public void checkJwt(JoinPoint joinPoint) {
+    public Object checkJwt(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
         //接收请求，记录请求
         ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         if (sra!=null){
@@ -47,7 +52,6 @@ public class AOP {
                 System.out.println("jwt is bad");
             }
 
-
             //记录日志
             System.out.println("url" + request.getRequestURI().toString());
             System.out.println("method" + request.getMethod());
@@ -60,7 +64,19 @@ public class AOP {
 
             }
         }
-
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        Object result = proceedingJoinPoint.proceed();
+        stopWatch.stop();
+        double elapsedTime = stopWatch.getTime() / 1000.0;
+        Signature signature = proceedingJoinPoint.getSignature();
+        String infoString = "[" + signature.toShortString() + "][Elapsed time: " + elapsedTime + " s]";
+//            if (elapsedTime > 1) {
+//                log.error(infoString + "[Note that it's time consuming!]");
+//            } else {
+//                log.info(infoString);
+//            }
+        return result;
     }
 
     /**
