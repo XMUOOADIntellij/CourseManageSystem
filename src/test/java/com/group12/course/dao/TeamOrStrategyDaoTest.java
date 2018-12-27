63af421c099a31e1f78fdad0d952f6007833e371
Xu Gang <xugang9804@outlook.com>
2018-12-28 03:34:04 +0800

    bug fixed

--- src/test/java/com/group12/course/dao/TeamOrStrategyDaoTest.java
+++ src/test/java/com/group12/course/dao/TeamOrStrategyDaoTest.java
@@ -1,19 +1,23 @@
 package com.group12.course.dao;
 
+import com.group12.course.entity.Team;
 import com.group12.course.entity.strategy.TeamOrStrategy;
+import com.group12.course.exception.InformationException;
 import com.group12.course.mapper.TeamOrStrategyMapper;
+import com.group12.course.service.TeamService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 @RunWith(SpringRunner.class)
-@MybatisTest
+@SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class TeamOrStrategyDaoTest {
 
@@ -21,6 +25,15 @@ public class TeamOrStrategyDaoTest {
     TeamOrStrategyMapper teamOrStrategyMapper;
     @Autowired
     TeamOrStrategyDao teamOrStrategyDao;
+
+    @Autowired
+    TeamService teamService;
+
+    @Autowired
+    MemberLimitStrategyDao memberLimitStrategyDao;
+
+    @Autowired
+    ConflictCourseStrategyDao conflictCourseStrategyDao;
 
     @Test
     public void testSelectTeamOrStrategyById(){
@@ -37,6 +50,17 @@ public class TeamOrStrategyDaoTest {
     public void testCalculateId(){
         Long id = teamOrStrategyDao.calculateId();
         Assert.assertEquals(new Long(2),id);
+    }
+
+
+    @Test
+    public void testCheck(){
+        Team team = teamService.getTeamByTeamId(6L);
+        Boolean status = teamService.checkTeamValidation(team);
+        if (!status){
+            throw new InformationException("队伍不合法");
+            // 记得此处提醒前端相关状态码为409
+        }
     }
 
 
